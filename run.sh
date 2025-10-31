5dc808a27016805dcfe36e747eec4a56808ccb14
omid-j-d <69404929+omid-j-d@users.noreply.github.com>
2025-10-31 14:40:31 +0330

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# تابعی برای گرفتن Ctrl+C
+trap ctrl_c INT
+
+ctrl_c() {
+    echo -e "\n⚡ Returning to main menu..."
+    sleep 1
+}
+
+while true; do
+    clear
+    echo "======================="
+    echo "   AIO Script Launcher "
+    echo "======================="
+    echo "1) adblocklist"
+    echo "4) Run script 2"
+    echo "3) Exit"
+    read -p "Choose an option [1-3]: " choice
+
+    case $choice in
+        1)
+            echo "🐍 Running Python script..."
+            # Ctrl+C داخل این اسکریپت باعث برگرداندن به منو میشه
+            python3 <(https://raw.githubusercontent.com/omid-j-d/junk-code/refs/heads/main/adblock.py) || true
+            ;;
+        2)
+            echo "🚀 Running Bash script..."
+            bash <(curl -fsSL https://raw.githubusercontent.com/omid-j-d/junk-code/main/setup-fastfetch.sh) || true
+            ;;
+        3)
+            echo "👋 Goodbye!"
+            exit 0
+            ;;
+        4)
+            echo "🐍 Running Python script..."
+            python3 <(https://raw.githubusercontent.com/omid-j-d/junk-code/refs/heads/main/nsfwblock.py) || true
+            ;;
+        *)
+            echo "❌ Invalid option, try again."
+            sleep 1
+            ;;
+    esac
+done
